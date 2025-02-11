0713f206c7e297e46f31016299ef5fc0b2194ee9
francia.jarufe@usach.cl <francia.jarufe@usach.cl>
2025-02-11 21:41:46 +0000

    Initial Import

---
+++ src/components/header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import video from '../components/2386461-hd_1920_1080_24fps.mp4'
+
+export const Header = (props) => {
+  return (
+    <header id="header">
+      <div className="intro">
+        <video
+          className="video-background"
+          playsInline
+          autoPlay
+          muted
+          loop
+          poster="/api/placeholder/1920/1080"
+        >
+          {/* Replace the src with your actual video file path */}
+          <source src={video} type="video/mp4" />
+        </video>
+        <div className="overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text">
+                <h1>
+                  {props.data ? props.data.title : "Loading"}
+                  <span></span>
+                </h1>
+                <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                {/* <a
+                  href="#features"
+                  className="btn btn-custom btn-lg page-scroll"
+                >
+                  Learn More
+                </a> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ src/components/navigation.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+export const Navigation = (props) => {
+  return (
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+          >
+            {" "}
+            <span className="sr-only">Toggle navigation</span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+          </button>
+          <a className="navbar-brand page-scroll" href="#page-top">
+            Francia Jarufe
+          </a>{" "}
+        </div>
+
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#features" className="page-scroll">
+                Inicio
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="page-scroll">
+                Sobre Mi
+              </a>
+            </li>
+            <li>
+              <a href="#services" className="page-scroll">
+                Estudiar Inglés 
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" className="page-scroll">
+                Estudiar en la Universidad
+              </a>
+            </li>
+            <li>
+              <a href="#testimonials" className="page-scroll">
+                Recursos Utiles
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="page-scroll">
+                Contacto
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
