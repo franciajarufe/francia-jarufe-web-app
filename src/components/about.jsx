f83deb5e688923c3df86a06bcb0aadaa2fda1878
Patricio's Mac <Patricio_G@tests-MacBook-Air.local>
2025-03-15 21:50:31 +0000

    elimine unos comentarios y modifique el titulo del navegador

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -21,28 +21,6 @@ export const About = (props) => {
               
               <h2 style={{fontSize: 21,fontWeight: 'inherit'}}> "Convierte tu sueño de estudiar en el extranjero con mi asesoría personalizada."</h2>
               <p style={{fontSize: 14,fontWeight: 'bolder'}}>Francia Jarufe</p>
-
-              {/* <h3>Como puedo ayudarte?</h3> */}
-              {/* <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-              </div> */}
             </div>
           </div>
         </div>
